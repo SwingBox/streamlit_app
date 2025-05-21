cb1fcdf0e7cc1108ad93938e44fb02a92f9d964c
SwingBox <nathan.vallet.pro@gmail.com>
2025-05-21 11:27:23 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/taxis.csv")
+
+
+st.title("Bienvenue sur le site de Nathan")
+
+
+tiécar = st.selectbox("Indiquez votre arrondissement de récupération",
+
+             df['pickup_borough'].unique()) 
+
+st.write(f"Tu as choisi : {tiécar}")
+
+images = {
+    "Manhattan": "https://assets3.thrillist.com/v1/image/1660860/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70",
+    "Queens": "https://www.nyc.fr/wp-content/uploads/2024/10/vue-aerienne_du_queens-scaled.jpg",
+    "Bronx": "https://freedomforallamericans.org/wp-content/uploads/2024/07/Bronx-neighborhood-crime-rates.jpg",
+    "Brooklyn": "https://api.time.com/wp-content/uploads/2011/12/r-young-brownsville7560.jpg",
+    "Staten Island": "https://s.wsj.net/public/resources/images/OB-VE476_1031st_G_20121031142247.jpg"
+}
+
+if tiécar in images:
+    st.image(images[tiécar], use_column_width=True)
+else:
+    st.image("https://fr.web.img6.acsta.net/c_310_420/pictures/23/09/01/18/03/4245127.jpg")
