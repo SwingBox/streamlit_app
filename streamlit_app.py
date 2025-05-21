ce51912944ea8c55bd514db0940c2a003c0328f0
SwingBox <nathan.vallet.pro@gmail.com>
2025-05-21 12:46:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ tiécar_options = sorted(set(tiécar_options))  # suppression des doublons et tr
 
 
 if "Staten Island" not in tiécar_options:
-    tiécar_options.append("Staten Island")
+    tiécar_options.append("Staten Island
+if "NaN" not in tiécar_options:
+    tiécar_options.append("NaN")
 
 tiécar = st.selectbox("Indiquez votre arrondissement de récupération", tiécar_options)
 
